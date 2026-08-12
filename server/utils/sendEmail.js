8e87a7ffb03a9ba3b9d1bfdbd814a7b870dd06d7
Muzammal Ghafoor <muzammalghafoor04@gmail.com>
2026-08-12 12:06:29 +0500

    Update Gmail SMTP configuration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,9 +3,9 @@ dotenv.config()
 import nodemailer from 'nodemailer'
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  port: 587,
+  secure: false,
+  
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
